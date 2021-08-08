df893ab0438e55c16edf1e726e1a24e6b3841859
Shtrauh Denis <denis.shtauh1987@gmail.com>
2021-08-08 18:43:06 +0300

    Commit1

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,12 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
+    @Bean(name="cat")
+    @Scope("prototype")
+    public Cat getCatPojo() {
+        Cat catPojo = new Cat();
+        catPojo.setName("cat");
+        return catPojo;
+    }
 }
